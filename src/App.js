90139baae0eb6cfb4549daf0975a045c66f4d368
TrungNNg <tnguy384@mail.ccsf.edu>
2020-10-21 11:29:31 -0400

    initial setup with github token and .env

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const axiosGithubGraphQL = axios.create({
+  baseURL: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
+
+class App extends React.Component {
+  state = {
+    path: "the-road-to-learn-react/the-road-to-learn-react",
+  };
+
+  componentDidMount() {}
+
+  onChange = (event) => {
+    this.setState({ path: event.target.value });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    const { path } = this.state;
+    return (
+      <div>
+        <div>Graphql Github Client App</div>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">Show open issues for https://github.com/</label>
+          <input
+            id="url"
+            type="text"
+            onChange={this.onChange}
+            style={{ width: "300px" }}
+            value={path}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default App;
